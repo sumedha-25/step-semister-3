29155ec97eeded974ba6fd13e98d8c5ed8d8db08
sumedha-25 <sumedhamehta25@gmail.com>
2026-09-21 15:48:57 +0530

    Add Week 5 Assignment 3

---
+++ Assignments/BookCopyCirculationGuard.java
@@ -0,0 +1,56 @@
+public class BookCopyCirculationGuard {
+
+    private int copiesTotal;
+    private int copiesAvailable;
+
+    public BookCopyCirculationGuard(int copiesTotal) {
+        if (copiesTotal <= 0) {
+            throw new IllegalArgumentException(
+                "Copies total must be positive"
+            );
+        }
+
+        this.copiesTotal = copiesTotal;
+        this.copiesAvailable = copiesTotal;
+    }
+
+    public void checkOut() {
+        if (copiesAvailable > 0) {
+            copiesAvailable--;
+        }
+    }
+
+    public void checkIn() {
+        if (copiesAvailable < copiesTotal) {
+            copiesAvailable++;
+        }
+    }
+
+    public int getCopiesAvailable() {
+        return copiesAvailable;
+    }
+
+    public static void main(String[] args) {
+
+        BookCopyCirculationGuard b =
+            new BookCopyCirculationGuard(3);
+
+        b.checkOut();
+        b.checkOut();
+        b.checkOut();
+        b.checkOut();
+
+        System.out.println(
+            b.getCopiesAvailable()
+        );
+
+        b.checkIn();
+        b.checkIn();
+        b.checkIn();
+        b.checkIn();
+
+        System.out.println(
+            b.getCopiesAvailable()
+        );
+    }
+}
