acf9a2e418c825969a0095cf7341fa72b3fc8e0d
sumedha-25 <sumedhamehta25@gmail.com>
2026-09-21 15:15:46 +0530

    Add Week 2 Practice Problem 2

---
+++ Practice Problems/CSVStudentRecordParser.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class CSVStudentRecordParser {
+
+    void parseStudentRecord(String csvLine) {
+
+        String[] fields = csvLine.split(",");
+
+        if (fields.length != 3) {
+            System.out.println("Invalid Record");
+        } else {
+            System.out.println("Name: " + fields[0]
+                    + " | Roll No: " + fields[1]
+                    + " | Dept: " + fields[2]);
+        }
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter student record: ");
+        String csvLine = sc.nextLine();
+
+        CSVStudentRecordParser obj = new CSVStudentRecordParser();
+        obj.parseStudentRecord(csvLine);
+
+        sc.close();
+    }
+}
